c078b0c39401ba5062361fe5b30e0df8cf3733df
Victor Gil <victor.gil.sepulveda@gmail.com>
2019-01-25 16:57:39 +0100

    Added Bank expenses to the model

--- gf_test/model/model.py
+++ gf_test/model/model.py
@@ -1,44 +1,61 @@
+import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, ForeignKey, Integer, String, Float
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, DateTime
 
 Base = declarative_base()
 
 
 class Bank(Base):
-    __tablename__ = 'bank'
-    id = Column(Integer, primary_key=True)
+    TABLE = 'bank'
+    __tablename__ = TABLE
+    id = Column(Integer, primary_key=True, autoincrement=True)
 
 
 class Account(Base):
     """
     A simple account
     """
-    __tablename__ = 'account'
-    id = Column(Integer, primary_key=True)
+    TABLE = 'account'
+    __tablename__ = TABLE
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    bank = Column(Integer, ForeignKey(Bank.TABLE+'.id'))
     amount = Column(Float, default=0.0)
 
 
 class Transfer(Base):
     """
-    A transfer between banks, holding some infoand associated expenses
+    A transfer between banks, holding some info and associated expenses
     """
-    __tablename__ = 'bank'
-    id = Column(Integer, primary_key=True)
-    sender_id = Column(Integer, ForeignKey('account.id'))
-    receiver_id = Column(Integer, ForeignKey('account.id'))
+    TABLE = 'transfer'
+    __tablename__ = TABLE
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    sender_id = Column(Integer, ForeignKey(Account.TABLE+'.id'))
+    receiver_id = Column(Integer, ForeignKey(Account.TABLE+'.id'))
     amount = Column(Float, nullable=False)
     info = Column(String(250))
-    cost = Column(Float(250), default=0.0)
+    cost = Column(Float, default=0.0)
+    created = Column(DateTime, default=datetime.datetime.utcnow)
+
+
+class BankExpense(Base):
+    """
+    Stores the expenses due to bank services. Now we only have transfers,
+    and we only want to store, so we can keep it simple.
+    """
+    __tablename__ = 'bankexpense'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    transfer = Column(Integer, ForeignKey(Transfer.TABLE + '.id'))
+    cost = Column(Float, default=0.0)
 
 
 class TransferCost(Base):
     """
     This stores the comission that each bank applies for a transfer
-    to another bank.
+    to another bank. Lookup table.
     """
-    sender_id = Column(Integer, ForeignKey('account.id'), primary_key=True)
-    receiver_id = Column(Integer, ForeignKey('account.id'), primary_key=True)
+    sender_bank_id = Column(Integer, ForeignKey(Bank.TABLE+'.id'), primary_key=True)
+    receiver_bank_id = Column(Integer, ForeignKey(Bank.TABLE+'.id'), primary_key=True)
     cost = Column(Float)
 
 engine = create_engine('sqlite:///getfinance_tech_test.db')
